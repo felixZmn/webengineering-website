37e92b59f376c103c93927722c4695779a1c038c
mmuellerluc <muellerl.tin21@student.dhbw-heidenheim.de>
2022-03-26 12:19:49 +0100

    Standort Seite

---
+++ scripts/standort.js
@@ -0,0 +1,107 @@
+var map;
+var layer_mapnik;
+var layer_tah;
+var layer_markers;
+
+function drawMap() {    
+    // Popup
+    var popuptext="<p><font color=\"black\"><b>DHBW Heidenheim<br>Marienstraße 20<br>89518 Heidenheim an der Brenz</b></p></font>";
+
+    OpenLayers.Lang.setCode('de');
+    
+    // Position und Zoomstufe der Karte
+    var lon = 10.154794576703763;
+    var lat = 48.68239911800074;
+    var zoom = 10;
+
+    map = new OpenLayers.Map('map', {
+        projection: new OpenLayers.Projection("EPSG:900913"),
+        displayProjection: new OpenLayers.Projection("EPSG:4326"),
+        controls: [
+            new OpenLayers.Control.Navigation(),
+            new OpenLayers.Control.LayerSwitcher(),
+            new OpenLayers.Control.PanZoomBar()],
+        maxExtent:
+            new OpenLayers.Bounds(-20037508.34,-20037508.34,
+                                    20037508.34, 20037508.34),
+        numZoomLevels: 18,
+        maxResolution: 156543,
+        units: 'meters'
+    });
+
+    layer_mapnik = new OpenLayers.Layer.OSM.Mapnik("Mapnik");
+    layer_markers = new OpenLayers.Layer.Markers("Address", { projection: new OpenLayers.Projection("EPSG:4326"), 
+    	                                          visibility: true, displayInLayerSwitcher: false });
+
+    map.addLayers([layer_mapnik, layer_markers]);
+    jumpTo(lon, lat, zoom);
+ 
+    // Position des Markers
+    addMarker(layer_markers, 10.154794576703763, 48.68239911800074, popuptext);
+
+}
+
+
+function jumpTo(lon, lat, zoom) {
+    var x = Lon2Merc(lon);
+    var y = Lat2Merc(lat);
+    map.setCenter(new OpenLayers.LonLat(x, y), zoom);
+    return false;
+}
+
+function Lon2Merc(lon) {
+    return 20037508.34 * lon / 180;
+}
+
+function Lat2Merc(lat) {
+    var PI = 3.14159265358979323846;
+    lat = Math.log(Math.tan( (90 + lat) * PI / 360)) / (PI / 180);
+    return 20037508.34 * lat / 180;
+}
+
+function addMarker(layer, lon, lat, popupContentHTML) {
+
+    var ll = new OpenLayers.LonLat(Lon2Merc(lon), Lat2Merc(lat));
+    var feature = new OpenLayers.Feature(layer, ll); 
+    feature.closeBox = true;
+    feature.popupClass = OpenLayers.Class(OpenLayers.Popup.FramedCloud, {minSize: new OpenLayers.Size(300, 180) } );
+    feature.data.popupContentHTML = popupContentHTML;
+    feature.data.overflow = "hidden";
+            
+    var marker = new OpenLayers.Marker(ll);
+    marker.feature = feature;
+
+    var markerClick = function(evt) {
+        if (this.popup == null) {
+            this.popup = this.createPopup(this.closeBox);
+            map.addPopup(this.popup);
+            this.popup.show();
+        } else {
+            this.popup.toggle();
+        }
+        OpenLayers.Event.stop(evt);
+    };
+    marker.events.register("mousedown", feature, markerClick);
+
+    layer.addMarker(marker);
+    map.addPopup(feature.createPopup(feature.closeBox));
+}
+
+function getCycleTileURL(bounds) {
+   var res = this.map.getResolution();
+   var x = Math.round((bounds.left - this.maxExtent.left) / (res * this.tileSize.w));
+   var y = Math.round((this.maxExtent.top - bounds.top) / (res * this.tileSize.h));
+   var z = this.map.getZoom();
+   var limit = Math.pow(2, z);
+
+   if (y < 0 || y >= limit)
+   {
+     return null;
+   }
+   else
+   {
+     x = ((x % limit) + limit) % limit;
+
+     return this.url + z + "/" + x + "/" + y + "." + this.type;
+   }
+}
